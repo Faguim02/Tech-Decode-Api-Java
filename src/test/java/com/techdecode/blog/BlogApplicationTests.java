4966a7571c9763920b9aa006a342ddae63ab2c8a
Faguim <fagnermuniz12@gmail.com>
2024-07-28 11:05:24 -0300

    first commit

---
+++ src/test/java/com/techdecode/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techdecode.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
