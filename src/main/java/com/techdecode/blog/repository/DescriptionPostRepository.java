b4903171d075ece5c2b745bbd1a511b4db02b1ea
Faguim <fagnermuniz12@gmail.com>
2024-07-28 19:05:00 -0300

    feat: repository/CommentRepository.java repository/DescriptionPostRepository create interface Repository

---
+++ src/main/java/com/techdecode/blog/repository/DescriptionPostRepository.java
@@ -0,0 +1,11 @@
+package com.techdecode.blog.repository;
+
+import com.techdecode.blog.models.DescriptionPostModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface DescriptionPostRepository extends JpaRepository<DescriptionPostModel, UUID> {
+}
