359650be80f65ea05ce965aa63bc017012ee26d7
Faguim <fagnermuniz12@gmail.com>
2024-07-29 15:44:38 -0300

    feat: create infra/security/SecurityFilter.java

--- src/main/java/com/techdecode/blog/repository/AdminRepository.java
+++ src/main/java/com/techdecode/blog/repository/AdminRepository.java
@@ -4,8 +4,11 @@ import com.techdecode.blog.models.AdminModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AdminRepository extends JpaRepository<AdminModel, UUID> {
+    Optional<AdminModel> findByEmail(String email);
 }
