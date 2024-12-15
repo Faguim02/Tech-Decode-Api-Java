93a8a22e07e19fd0f93d69ebb34a94c30dbc8c98
Faguim02 <fagnermuniz12@gmail.com>
2024-12-15 15:01:01 -0300

    feat✨: create new entity category

--- src/main/java/com/techdecode/blog/models/CategoryModel.java
+++ src/main/java/com/techdecode/blog/models/CategoryModel.java
@@ -13,4 +13,28 @@ public class CategoryModel {
     private String title;
     @OneToMany(mappedBy = "category")
     private List<PostModel> postModels;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<PostModel> getPostModels() {
+        return postModels;
+    }
+
+    public void setPostModels(List<PostModel> postModels) {
+        this.postModels = postModels;
+    }
 }
