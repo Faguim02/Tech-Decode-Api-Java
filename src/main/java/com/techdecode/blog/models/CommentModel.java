f9af33f7f24485913cf0f12d6fc36d4ed7785bf8
Faguim02 <fagnermuniz12@gmail.com>
2024-12-15 15:58:40 -0300

    feat✨: create CommentModel.java

---
+++ src/main/java/com/techdecode/blog/models/CommentModel.java
@@ -0,0 +1,65 @@
+package com.techdecode.blog.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "comment")
+public class CommentModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String comment;
+    private String date_at;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private UserModel user;
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    @JsonIgnore
+    private PostModel post;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getDate_at() {
+        return date_at;
+    }
+
+    public void setDate_at(String date_at) {
+        this.date_at = date_at;
+    }
+
+    public UserModel getUser() {
+        return user;
+    }
+
+    public void setUser(UserModel user) {
+        this.user = user;
+    }
+
+    public PostModel getPost() {
+        return post;
+    }
+
+    public void setPost(PostModel post) {
+        this.post = post;
+    }
+}
