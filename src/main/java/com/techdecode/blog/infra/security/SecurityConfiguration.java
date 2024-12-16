3d761b09d3f0abb80e63f31e4aa8c324872bcb9e
Faguim02 <fagnermuniz12@gmail.com>
2024-12-16 17:31:46 -0300

    chore 🔧: config password encode and authenticationMenager

--- src/main/java/com/techdecode/blog/infra/security/SecurityConfiguration.java
+++ src/main/java/com/techdecode/blog/infra/security/SecurityConfiguration.java
@@ -1,25 +1,44 @@
 package com.techdecode.blog.infra.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> {
                     authorize.requestMatchers(HttpMethod.GET, "post").permitAll();
-                    authorize.requestMatchers(HttpMethod.POST, "auth").permitAll();
+                    authorize.requestMatchers(HttpMethod.POST, "auth/*").permitAll();
                     authorize.requestMatchers(HttpMethod.POST, "comment").authenticated();
                     authorize.requestMatchers(HttpMethod.DELETE, "comment").authenticated();
                     authorize.anyRequest().hasRole("admin");
                 })
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
