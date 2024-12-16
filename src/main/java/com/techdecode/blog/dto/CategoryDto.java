811e9cb2c55cffa1ef1d27afd6bccdbb5db2326d
Faguim02 <fagnermuniz12@gmail.com>
2024-12-16 18:35:38 -0300

    feat ✨: create CategoryDto

---
+++ src/main/java/com/techdecode/blog/dto/CategoryDto.java
@@ -0,0 +1,13 @@
+package com.techdecode.blog.dto;
+
+import com.techdecode.blog.models.PostModel;
+
+import java.util.List;
+import java.util.UUID;
+
+public record CategoryDto(
+        UUID id,
+        String name,
+        List<PostModel> postModels
+) {
+}
