ac20a3413e52e74957143c900bca1e6629dc05ca
Faguim02 <fagnermuniz12@gmail.com>
2024-12-16 18:29:21 -0300

    feat ✨: inject repository and annotion class

--- src/main/java/com/techdecode/blog/service/CategoryService.java
+++ src/main/java/com/techdecode/blog/service/CategoryService.java
@@ -1,4 +1,11 @@
 package com.techdecode.blog.service;
 
+import com.techdecode.blog.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
 }
