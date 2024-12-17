0defb01d83cebdb15a58a49f91172768402393a1
Faguim02 <fagnermuniz12@gmail.com>
2024-12-17 20:50:59 -0300

    feat ✨: create a new method findAllPost

--- src/main/java/com/techdecode/blog/service/PostService.java
+++ src/main/java/com/techdecode/blog/service/PostService.java
@@ -3,6 +3,7 @@ package com.techdecode.blog.service;
 import com.techdecode.blog.dto.PostDto;
 import com.techdecode.blog.models.PostModel;
 import com.techdecode.blog.models.exceptions.ConflictException;
+import com.techdecode.blog.models.exceptions.NotFoundException;
 import com.techdecode.blog.repository.PostRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,5 +28,18 @@ public class PostService {
 
         PostModel postModelRes = this.postRepository.save(postModel);
         return new PostDto(postModelRes.getId(), postModelRes.getTitle(), postModelRes.getBannerUrl(), postModelRes.getDescription(), postModelRes.getFont(), postModelRes.getDate_at(), null, null);
+    }
+
+    public List<PostDto> findAllPost() {
+
+        List<PostModel> postModels = this.postRepository.findAll();
+
+        if (postModels.isEmpty()) {
+            throw new NotFoundException("a lista de postagem está vazia");
+        }
+
+        return postModels.stream()
+                .map(postModel -> new PostDto(postModel.getId(), postModel.getTitle(), postModel.getBannerUrl(), postModel.getDescription(), postModel.getFont(), postModel.getDate_at(), null, null))
+                .toList();
     }
 }
