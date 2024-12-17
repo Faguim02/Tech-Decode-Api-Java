09659da72a57698b1acc5df0fcba6daa4a124d52
Faguim02 <fagnermuniz12@gmail.com>
2024-12-16 21:10:55 -0300

    feat ✨: create method findAllCategory()

--- src/main/java/com/techdecode/blog/service/CategoryService.java
+++ src/main/java/com/techdecode/blog/service/CategoryService.java
@@ -1,11 +1,30 @@
 package com.techdecode.blog.service;
 
+import com.techdecode.blog.dto.CategoryDto;
+import com.techdecode.blog.models.CategoryModel;
 import com.techdecode.blog.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
+
+    public List<CategoryDto> findAllCategory() {
+        List<CategoryModel> categorys = categoryRepository.findAll();
+
+        List<CategoryDto> categoryDtos = categorys
+                .stream()
+                .map(categoryModel -> new CategoryDto(categoryModel.getId(), categoryModel.getTitle(), null))
+                .toList();
+
+        if (categoryDtos.size() == 0) {
+            // todo exception
+        }
+
+        return categoryDtos;
+    }
 }
