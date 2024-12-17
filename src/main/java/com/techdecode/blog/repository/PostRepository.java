b94b3f7803b63fa7df959f427064dc07102a5a61
Faguim02 <fagnermuniz12@gmail.com>
2024-12-17 12:21:36 -0300

    feat ✨: implements method findByTitle

--- src/main/java/com/techdecode/blog/repository/PostRepository.java
+++ src/main/java/com/techdecode/blog/repository/PostRepository.java
@@ -10,4 +10,5 @@ import java.util.UUID;
 @Repository
 public interface PostRepository extends JpaRepository<PostModel, UUID> {
     public List<PostModel> findByTitleContaining(String keyword);
+    public PostModel findByTitle(String title);
 }
