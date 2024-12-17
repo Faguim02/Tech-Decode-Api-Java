9a214d95ed709b700af10661e13cdac3292c064b
Faguim02 <fagnermuniz12@gmail.com>
2024-12-17 12:20:40 -0300

    feat ✨: create PostDto

--- src/main/java/com/techdecode/blog/dto/PostDto.java
+++ src/main/java/com/techdecode/blog/dto/PostDto.java
@@ -1,4 +1,17 @@
 package com.techdecode.blog.dto;
 
-public class PostDto {
+import java.util.List;
+import java.util.UUID;
+
+public record PostDto(
+        UUID id,
+        String title,
+        String bannerUrl,
+        String description,
+        String font,
+        String data_at,
+        List<Object> comments,
+        CategoryDto category
+) {
+
 }
