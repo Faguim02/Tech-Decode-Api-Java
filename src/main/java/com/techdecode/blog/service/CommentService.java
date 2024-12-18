75571428d69b9f8d7a169ca3637daa7df3513978
Faguim02 <fagnermuniz12@gmail.com>
2024-12-18 18:25:52 -0300

    feat ✨: create class CommentService

---
+++ src/main/java/com/techdecode/blog/service/CommentService.java
@@ -0,0 +1,11 @@
+package com.techdecode.blog.service;
+
+import com.techdecode.blog.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+}
