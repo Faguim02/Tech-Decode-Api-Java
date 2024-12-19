1e915d716642e7c4f9ad1aaae4de6e37ae6efbaf
Faguim02 <fagnermuniz12@gmail.com>
2024-12-19 20:24:56 -0300

    refactor ♻️: create relaction user_id and post_id for comment

--- src/main/java/com/techdecode/blog/service/CommentService.java
+++ src/main/java/com/techdecode/blog/service/CommentService.java
@@ -2,8 +2,12 @@ package com.techdecode.blog.service;
 
 import com.techdecode.blog.dto.CommentDto;
 import com.techdecode.blog.models.CommentModel;
+import com.techdecode.blog.models.PostModel;
+import com.techdecode.blog.models.UserModel;
 import com.techdecode.blog.models.exceptions.ForbiddenException;
 import com.techdecode.blog.repository.CommentRepository;
+import com.techdecode.blog.repository.PostRepository;
+import com.techdecode.blog.repository.UserRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,11 +19,25 @@ import java.util.UUID;
 public class CommentService {
     @Autowired
     private CommentRepository commentRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PostRepository postRepository;
 
-    CommentDto createComment(CommentDto commentDto) {
+    CommentDto createComment(CommentDto commentDto, UUID user_id, UUID post_id) {
         CommentModel commentModel = new CommentModel();
 
         BeanUtils.copyProperties(commentDto, commentModel);
+
+        Optional<UserModel> userModelOptional = this.userRepository.findById(user_id);
+        Optional<PostModel> postModelOptional = this.postRepository.findById(post_id);
+
+        if (userModelOptional.isEmpty() || postModelOptional.isEmpty()) {
+            throw new ForbiddenException("você não foi autorizado para isso");
+        }
+
+        commentModel.setPost(postModelOptional.get());
+        commentModel.setUser(userModelOptional.get());
 
         CommentModel commentModelRes = this.commentRepository.save(commentModel);
 
