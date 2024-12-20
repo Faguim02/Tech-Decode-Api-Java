6d50692e66078b323be1623daf4a4b0f6227f071
Faguim02 <fagnermuniz12@gmail.com>
2024-12-19 21:36:44 -0300

    test ✅: create a new test 'should return commentDto'

--- src/test/java/com/techdecode/blog/service/CommentServiceTest.java
+++ src/test/java/com/techdecode/blog/service/CommentServiceTest.java
@@ -1,14 +1,25 @@
 package com.techdecode.blog.service;
 
+import com.techdecode.blog.dto.CommentDto;
+import com.techdecode.blog.models.CommentModel;
+import com.techdecode.blog.models.PostModel;
+import com.techdecode.blog.models.UserModel;
 import com.techdecode.blog.repository.CommentRepository;
 import com.techdecode.blog.repository.PostRepository;
 import com.techdecode.blog.repository.UserRepository;
+import jakarta.persistence.Table;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @DisplayName("test: comment service")
 @ExtendWith(MockitoExtension.class)
@@ -27,6 +38,26 @@ public class CommentServiceTest {
     @Nested
     @DisplayName("test method: createComment")
     class CreateComment {
+
+        @DisplayName("should return CommentDto")
+        @Test
+        void shouldReturnCommentDto() {
+            UUID user_id = UUID.randomUUID();
+            UUID post_id = UUID.randomUUID();
+
+            UserModel userModel = new UserModel();
+            PostModel postModel = new PostModel();
+            CommentModel commentModel = new CommentModel();
+            CommentDto commentDto = new CommentDto(UUID.randomUUID(), "title", "19 dez 2024", userModel, postModel);
+
+            Mockito.when(userRepository.findById(Mockito.any(UUID.class))).thenReturn(Optional.of(userModel));
+            Mockito.when(postRepository.findById(Mockito.any(UUID.class))).thenReturn(Optional.of(postModel));
+            Mockito.when(commentRepository.save(Mockito.any(CommentModel.class))).thenReturn(commentModel);
+
+            CommentDto commentDtoRes = commentService.createComment(commentDto, user_id, post_id);
+
+            Assertions.assertNotNull(commentDtoRes);
+        }
 
     }
 
