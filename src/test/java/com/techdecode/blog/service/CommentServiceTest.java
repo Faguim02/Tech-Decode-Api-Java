6710f571d2928dc48f8a8f65a8b5dba3af164dc1
Faguim02 <fagnermuniz12@gmail.com>
2024-12-19 21:39:45 -0300

    test ✅: create a new test 'should return forbiddenException'

--- src/test/java/com/techdecode/blog/service/CommentServiceTest.java
+++ src/test/java/com/techdecode/blog/service/CommentServiceTest.java
@@ -4,6 +4,7 @@ import com.techdecode.blog.dto.CommentDto;
 import com.techdecode.blog.models.CommentModel;
 import com.techdecode.blog.models.PostModel;
 import com.techdecode.blog.models.UserModel;
+import com.techdecode.blog.models.exceptions.ForbiddenException;
 import com.techdecode.blog.repository.CommentRepository;
 import com.techdecode.blog.repository.PostRepository;
 import com.techdecode.blog.repository.UserRepository;
@@ -57,6 +58,22 @@ public class CommentServiceTest {
             CommentDto commentDtoRes = commentService.createComment(commentDto, user_id, post_id);
 
             Assertions.assertNotNull(commentDtoRes);
+        }
+
+        @DisplayName("should return forbiddenException")
+        @Test
+        void shouldReturnForbiddenException() {
+            UUID user_id = UUID.randomUUID();
+            UUID post_id = UUID.randomUUID();
+
+            UserModel userModel = new UserModel();
+            PostModel postModel = new PostModel();
+            CommentDto commentDto = new CommentDto(UUID.randomUUID(), "title", "19 dez 2024", userModel, postModel);
+
+            Mockito.when(userRepository.findById(Mockito.any(UUID.class))).thenReturn(Optional.empty());
+            Mockito.when(postRepository.findById(Mockito.any(UUID.class))).thenReturn(Optional.of(postModel));
+
+            Assertions.assertThrows(ForbiddenException.class, () -> commentService.createComment(commentDto, user_id, post_id));
         }
 
     }
