5c0f997dda71fef22a8d8462c10046fd7b04b819
Faguim02 <fagnermuniz12@gmail.com>
2024-12-22 11:53:03 -0300

    feat 🔒️: add securityFilter in SecurityConfiguration

--- src/main/java/com/techdecode/blog/infra/security/SecurityConfiguration.java
+++ src/main/java/com/techdecode/blog/infra/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.techdecode.blog.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,10 +13,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -29,6 +34,7 @@ public class SecurityConfiguration {
                     authorize.requestMatchers(HttpMethod.DELETE, "comment").authenticated();
                     authorize.anyRequest().hasRole("admin");
                 })
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
---
+++ src/main/java/com/techdecode/blog/infra/security/SecurityFilter.java
@@ -0,0 +1,44 @@
+package com.techdecode.blog.infra.security;
+
+import com.techdecode.blog.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtService tokenService;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recorverToken(request);
+        if (token != null) {
+            var email = tokenService.validToken(token);
+            UserDetails userDetails = this.userRepository.findByEmail(email);
+            var auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recorverToken(HttpServletRequest request) {
+        var authToken = request.getHeader("Authorization");
+        if (authToken == null) return null;
+
+        return authToken.replace("Bearer ", "");
+    }
+}
