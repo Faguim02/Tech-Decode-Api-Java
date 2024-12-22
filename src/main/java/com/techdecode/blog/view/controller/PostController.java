a26f148dc3c2458c8a7484e80cae46961d26e24a
Faguim02 <fagnermuniz12@gmail.com>
2024-12-22 17:05:33 -0300

    feat ✨: create gets 'findAll', 'findById', 'search', 'findByCategory'

--- src/main/java/com/techdecode/blog/view/controller/PostController.java
+++ src/main/java/com/techdecode/blog/view/controller/PostController.java
@@ -1,17 +1,20 @@
 package com.techdecode.blog.view.controller;
 
+import com.techdecode.blog.dto.CategoryDto;
 import com.techdecode.blog.dto.PostDto;
 import com.techdecode.blog.service.PostService;
 import com.techdecode.blog.view.model.post.PostCreateRequest;
 import com.techdecode.blog.view.model.post.PostCreateResponse;
+import com.techdecode.blog.view.model.post.PostResponse;
+import com.techdecode.blog.view.model.post.PostResponseDetails;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("post")
@@ -29,5 +32,42 @@ public class PostController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(postCreateResponse);
     }
+
+    @GetMapping
+    ResponseEntity<List<PostResponse>> findAllPost() {
+        List<PostDto> postDtos = this.postService.findAllPost();
+        List<PostResponse> postResponses = postDtos.stream()
+                .map(postDto -> new PostResponse(postDto.id(), postDto.title(), postDto.bannerUrl(), postDto.data_at()))
+                .toList();
+
+        return ResponseEntity.status(HttpStatus.OK).body(postResponses);
+    }
+
+    @GetMapping("{id}")
+    ResponseEntity<PostResponseDetails> findPostById(@PathVariable("id") UUID id) {
+        PostDto postDto = this.postService.findPostById(id);
+        PostResponseDetails postResponseDetails = new PostResponseDetails(postDto.id(), postDto.title(), postDto.bannerUrl(), postDto.description(), postDto.font(), postDto.data_at(), postDto.comments(), null);
+
+        return ResponseEntity.status(HttpStatus.OK).body(postResponseDetails);
+    }
+
+    @GetMapping("search/{search}")
+    ResponseEntity<List<PostResponse>> searchPost(@PathVariable("search") String search) {
+        List<PostDto> postDtos = this.postService.searchPost(search);
+        List<PostResponse> postResponses = postDtos.stream()
+                .map(postDto -> new PostResponse(postDto.id(), postDto.title(), postDto.bannerUrl(), postDto.data_at()))
+                .toList();
+
+        return ResponseEntity.status(HttpStatus.OK).body(postResponses);
+    }
+
+    @GetMapping("category/{id}")
+    ResponseEntity<CategoryDto> findPostByCategory(@PathVariable("id") UUID id) {
+        CategoryDto categoryDto = this.postService.findPostByCategory(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(categoryDto);
+    }
+
+
 
 }
