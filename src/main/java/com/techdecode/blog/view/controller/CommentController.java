21086e7c3f55e3ce95d9edbdbd2232d670593f65
Faguim02 <fagnermuniz12@gmail.com>
2024-12-28 19:40:03 -0300

    📚 docs: ajuste na documentação swagger, inserindo o @ApiResponse em todos

--- src/main/java/com/techdecode/blog/view/controller/CommentController.java
+++ src/main/java/com/techdecode/blog/view/controller/CommentController.java
@@ -5,11 +5,16 @@ import com.techdecode.blog.service.CommentService;
 import com.techdecode.blog.view.model.comment.CommentRequest;
 import com.techdecode.blog.view.model.comment.CommentResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -22,7 +27,13 @@ public class CommentController {
     @Autowired
     private CommentService commentService;
 
+
+    // Criar comentario
     @Operation(summary = "criar comentario", description = "nessa rota será criado um comentario que foi informado no body da requisição")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "comentario criado", content = @Content(schema = @Schema(implementation = CommentResponse.class))),
+            @ApiResponse(responseCode = "403", description = "você não está authenticado", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping
     ResponseEntity<CommentResponse> createComment(@RequestBody CommentRequest body) {
         String email = (String) SecurityContextHolder.getContext().getAuthentication().getName();
@@ -35,7 +46,13 @@ public class CommentController {
         return ResponseEntity.status(HttpStatus.CREATED).body(commentResponse);
     }
 
+
+    // Deletar comentario
     @Operation(summary = "apagar comentario", description = "nessa rota, o usuario informa o id do comentario, e com base no seu jwt, será extarido de você é o criador do comentario, e por fim, será deletado")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "comentario deletado", content = @Content(schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "404", description = "comentario do id inserido não existe", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @DeleteMapping("{id}")
     ResponseEntity<String> deleteComment(@PathVariable("id") UUID comment_id) {
         String email = (String) SecurityContextHolder.getContext().getAuthentication().getName();
