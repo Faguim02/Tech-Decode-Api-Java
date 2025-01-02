cf452df79a3aa47badbadbc533d6bc30318f5b02
Faguim02 <fagnermuniz12@gmail.com>
2025-01-02 11:18:47 -0300

    🗃️ raw: alterando script sql

--- database/init.sql
+++ database/init.sql
@@ -1,1 +1,7 @@
-CREATE DATABASE techdecode;
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'techdecode') THEN
+        CREATE DATABASE techdecode;
+    END IF;
+END
+$$;
