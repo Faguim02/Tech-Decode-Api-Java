e26976c7c1132afa74d1eade17fa26c8de97d477
Faguim02 <fagnermuniz12@gmail.com>
2025-01-19 18:03:51 -0300

    ✨ feat: adicionando method que busca por todos os usuarios no banco de dados conforme o userRole

--- src/main/java/com/techdecode/blog/repository/UserRepository.java
+++ src/main/java/com/techdecode/blog/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.techdecode.blog.repository;
 
 import com.techdecode.blog.models.UserModel;
+import com.techdecode.blog.models.roles.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,5 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
     UserDetails findByEmail(String email);
+    UserModel findByUserRole(UserRole userRole);
 }
