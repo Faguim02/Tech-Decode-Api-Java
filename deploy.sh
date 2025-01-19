8e6f60c74a24bdb9f68b6dc7d009d29b07a7ae67
Faguim02 <fagnermuniz12@gmail.com>
2025-01-19 20:46:21 -0300

    🚀 editando arquivo de deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+echo "# Atualizando repositorio GIT..."
 git pull
+echo "------------------------------"
+echo "# Derrubando containeres existentes..."
 docker-compose down
-docker-compose up -d --build
+docker rmi -f tech-decode-spring
+echo "------------------------------"
+echo "# Criando a imagem..."
+docker build -t tech-decode-spring .
+echo "------------------------------"
+echo "# Subindo o container..."
+docker-compose up -d
+echo "# Concluido com sucesso!!"
