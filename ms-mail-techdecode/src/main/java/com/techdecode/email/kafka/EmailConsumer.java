35936f4ce3796d7aebb9a228bc5d15e306ef3bd1
Faguim02 <fagnermuniz12@gmail.com>
2025-02-13 14:02:31 -0300

    ✨ feat: chamando o serviço que faz o envio do email

--- ms-mail-techdecode/src/main/java/com/techdecode/email/kafka/EmailConsumer.java
+++ ms-mail-techdecode/src/main/java/com/techdecode/email/kafka/EmailConsumer.java
@@ -2,19 +2,25 @@ package com.techdecode.email.kafka;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.techdecode.email.dto.EmailDto;
+import com.techdecode.email.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailConsumer {
+
+    @Autowired
+    private EmailService emailService;
+
     @KafkaListener(topics = "email-topic" ,groupId = "group-mail-consumer")
     public void receiveMessage(String email) {
         try{
             ObjectMapper objectMapper = new ObjectMapper();
             EmailDto emailDto = objectMapper.readValue(email, EmailDto.class);
-            System.out.println(emailDto.emailTo());
+            this.emailService.sendToEmail(emailDto);
         }catch (Exception e) {
-            System.out.println("ops");
+            System.out.println("Erro ao receber dados do producer");
         }
     }
 }
